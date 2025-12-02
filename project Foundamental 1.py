f19c23096cace587841c9b59380f8ea9d84908e7
jayalrami <jayalgorajiya@gmail.com>
2025-12-02 10:58:45 +0530

    Add files via upload

---
+++ project Foundamental 1.py
@@ -0,0 +1,40 @@
+print("Welcome to the Interactive Personal Data Collector!")
+
+print("\n***************")
+
+
+name = input("Please enter your name:")
+
+age = int(input("Please enter your age:"))
+
+height = float(input("Please enter your height:"))
+
+number = int(input("Please enter your number:"))
+
+print("\n***************")
+
+
+print("Thank you! Here is the information We collected!")
+
+
+print(f" Name:{name}(Type: {type(name)},Memory Address:{id(name)}")
+
+print(f" Age:{age}(Type: {type(age)},Memory Address:{id(age)}")
+
+print(f" Height:{height}(Type: {type(height)},Memory Address:{id(height)}")
+
+print(f" Number:{number}(Type: {type(number)},Memory Address:{id(number)}")
+
+
+
+birthday = 2025-age
+
+
+
+print(f" Your birth year is approximat year:{birthday}(based on year age of {age})")
+
+
+
+print("Thank you for using the personal Data Collector.Goodbye!")
+
+      
